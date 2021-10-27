d99b9f0f9428f808145763a721a48eb7bd07de04
Mariano <marianoguglieri@gmail.com>
2021-10-27 13:57:30 -0300

    mapa no tan en pañales

--- main.cpp
+++ main.cpp
@@ -4,116 +4,14 @@
 #include "mapa.h"
 
 using namespace std;
-/*
-class Material{
-    string nombre = "Madera";
-    int cantidad = 60;
-    public: 
-        string mostrar_nombre()
-        {return nombre;}
-        int mostrar_cantidad()
-        {return cantidad;}
-};
-
-class Casillero{
-  protected:
-    int coord_x, coord_y;
-  public:
-   void set_coordenadas(int x,int y)
-   {coord_x = x; coord_y = y;}
-   virtual void mostrar() = 0; 
-   virtual void agregar_material(Material){};
-};
-
-class CasilleroConstruible: public Casillero {
-  public:
-    void mostrar()
-      { cout << "Soy un casillero construible" << endl;
-        cout << "estás en la posición " << coord_x << " " << coord_y << endl;}
-};
-
-
-
-class Edificio{
-    string nombre = "aserradero";
-    public: 
-        string mostrar_nombre()
-        {return nombre;}
-};
-
-class CasilleroTransitable: public Casillero{
-  Material material;
-  public:
-    void mostrar()
-    { cout << "Soy un casillero transitable y tengo " << material.mostrar_cantidad() << " de "
-    << material.mostrar_nombre() << endl;
-    cout << "estás en la posición " << coord_x << " " << coord_y << endl;}
-};
-
-class CasilleroInaccesible: public Casillero{
-    public:
-        void mostrar()
-        {cout << "Soy un casillero inaccesible y estás en la posición "<< coord_x << " " << coord_y << endl;}
-};
-*/
-
 
 int main (){
 
-  //Casillero *mapa[2][2];
+  Mapa andypolis("mapa.txt");
 
-  Mapa mapa(8,10);
+  andypolis.consultar_coordenada(0,0);
 
-  mapa.cargar();
+  andypolis.mostrar_mapa();
 
-  mapa.display();
-
-/*
-  CasilleroConstruible casillero_construible;
-  CasilleroInaccesible casillero_inaccesible;
-  CasilleroTransitable casillero_transitable;
-
-  int filas = 5;
-  int columnas = 5;
-  
-  Casillero* **mapa;
-  mapa = new Casillero**[filas];
-  for (int i = 0; i < filas; i++){
-		mapa[i] = new Casillero*[columnas];
-    for(int j = 0; j < columnas; j++){
-			mapa[i][j] = &casillero_construible;
-      mapa[i][j]->set_coordenadas(i,j);
-      mapa[i][j]->mostrar();
-    }
-	}
-   for (int i = 0; i < filas; i++){
-    for(int j = 0; j < columnas; j++){
-      mapa[i][j]->mostrar();
-    }
-	}
-  for (int i = 0; i < filas; i++)
-	    delete [] mapa[i];
-
-  delete[] mapa;
-*/
-/*
-  mapa[0][0] = new CasilleroConstruible;
-  mapa[0][0]->set_coordenadas(0,0);
-
-  mapa[1][0] = new CasilleroInaccesible;
-  mapa[1][0]->set_coordenadas(1,0);
-
-  
-  mapa[2][0] = new CasilleroTransitable;
-  mapa[2][0]->set_coordenadas(2,0);
-
-  mapa[0][0]->mostrar();
-  mapa[1][0]->mostrar();
-  mapa[2][0]->mostrar();
-
-  delete mapa[0][0];
-  delete mapa[1][0];
-  delete mapa[2][0];
-*/
   return 0;
 }
--- mapa.h
+++ mapa.h
@@ -10,59 +10,77 @@ class Mapa{
   
     Casillero* **mapa;
 
-    CasilleroConstruible casillero_construible;
-    CasilleroInaccesible casillero_inaccesible;
-    CasilleroTransitable casillero_transitable;
-
     public: 
 
-    Mapa(int x, int y){
+        Mapa():filas(0),columnas(0),mapa(0){}
+        Mapa(const string &);
+        ~Mapa();
 
-        filas = x;
-        columnas = y; 
-
-        mapa = new Casillero**[filas];
-
-        for (int i = 0; i < filas; i++){
-		    mapa[i] = new Casillero*[columnas];
-            /*for(int j = 0; j < columnas; j++){
-			    mapa[i][j] = &casillero_construible;
-                mapa[i][j]->set_coordenadas(i,j);
-            }*/
-	    }
-    } 
-        
-    void cargar(){
-        for (int i = 0; i < filas; i++){
-            for(int j = 0; j < columnas; j++){
-			    mapa[i][j] = &casillero_construible;
-                mapa[i][j]->set_coordenadas(i,j);
-            }
-	    }
-    }
-    ~Mapa(){ 
-        for (int i = 0; i < filas; i++)
-	        delete [] mapa[i];
-
-        delete[] mapa;
-    }
-
-    void mostrar(){
-         for (int i = 0; i < filas; i++){
-            for(int j = 0; j < columnas; j++){
-                mapa[i][j]->mostrar();
-            }
-         }
-	}
-
-     void display(){
-         for (int i = 0; i < filas; i++){
-            for(int j = 0; j < columnas; j++){
-                mapa[i][j]->display();
-            }
-            cout << endl;
-         }
-	}
+        void consultar_coordenada(int, int);
+        void mostrar_mapa();
 };
 
+Mapa::Mapa(const string & PATH){
 
+    fstream archivo_mapa(PATH, ios::in);
+
+    if(!archivo_mapa.is_open()){
+        cout << "No se encontro un archivo con nombre \"" << PATH << "\", se va a crear el archivo" << endl;
+        archivo_mapa.open(PATH, ios::out);
+        archivo_mapa.close();
+        archivo_mapa.open(PATH, ios::in);
+    }
+
+    string casillero, filas_, columnas_;
+
+    archivo_mapa >> filas_;
+    archivo_mapa >> columnas_;
+    filas = stoi(filas_);
+    columnas = stoi(columnas_);
+
+    mapa = new Casillero**[filas];
+
+    for (int i = 0; i < filas; i++){
+		mapa[i] = new Casillero*[columnas];
+        for(int j = 0; j < columnas; j++){
+            archivo_mapa >> casillero;
+            if(casillero == "L"){
+                mapa[i][j] = new CasilleroInaccesible;
+                mapa[i][j]->set_coordenadas(i,j);
+            }
+            if(casillero == "T"){
+			    mapa[i][j] = new CasilleroConstruible;
+                mapa[i][j]->set_coordenadas(i,j);
+            }
+            if(casillero == "C"){
+			    mapa[i][j] = new CasilleroTransitable;
+                mapa[i][j]->set_coordenadas(i,j);
+            }
+        }
+	}
+}
+
+Mapa::~Mapa(){ 
+
+    for (int i = 0; i < filas; i++)
+        for (int j = 0; j < columnas; j++)
+	        delete mapa[i][j];
+
+    for (int i = 0; i < filas; i++)
+	    delete [] mapa[i];
+
+    delete[] mapa;
+}
+
+Mapa::void consultar_coordenada(int i, int j){
+    mapa[i][j]->mostrar();
+}
+
+Mapa::void mostrar_mapa(){
+    for (int i = 0; i < filas; i++){
+        for(int j = 0; j < columnas; j++){
+            mapa[i][j]->display();
+        }
+        cout << endl;
+    }
+}
