6d1c7fd84533105b3f68a067d59b7d4d51d2456f
Lucia Berard <lberard@fi.uba.ar>
2021-11-01 00:59:46 -0300

    nuevo main(comentado)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include "menu.h"
-#include "mapa.h"
-#include "material.h"
+#include "menu.hpp"
+#include "mapa.hpp"
+#include "material.hpp"
 
 using namespace std;
 
@@ -11,7 +11,35 @@ int main()
 {
 
     /*
-    ACA SE CREARIAN LOS VECTORES Y EL MAPA
+    //ACA SE CREARIAN LOS VECTORES Y EL MAPA
+
+    Parser parser;
+
+    Lista<Edificio> edificios;
+    parser.cargar(edificios,"edificios.txt");
+
+    //ESTO FALTARIA
+    // parser.cargar_ubicaciones(edificios,"ubicaciones.txt");
+
+    Lista<Material> materiales;
+    parser.cargar(materiales, "materiales.txt");
+
+    Edificio *edificio1 = edificios.consulta(1);
+    Edificio *edificio2 = edificios.consulta(2);
+
+    cout << edificio1->mostrar() << endl;
+    cout << edificio2->mostrar() << endl;
+
+    Material *material1 = materiales.consulta(1);
+    Material *material2 = materiales.consulta(2);
+
+    cout << materiales.consulta(2)->mostrar() << endl;
+
+    cout << material1->mostrar() << endl;
+    cout << material2->mostrar() << endl;
+
+    andypolis.agregar_edificio(0, 3, edificios.consulta(1));
+    andypolis.consultar_coordenada(0, 3);
     */
 
     Mapa andypolis("mapa.txt");
