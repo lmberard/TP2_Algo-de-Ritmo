1de0fa1321b2cf18195f9dc48e974b5c270ec3b8
marianoguglieri <55066532+marianoguglieri@users.noreply.github.com>
2021-11-03 09:58:58 -0300

    Create edificioAserradero.cpp

---
+++ edificioAserradero.cpp
@@ -0,0 +1,14 @@
+
+#include "edificio.hpp"
+#include "edificioAserradero.hpp"
+Aserradero ::Aserradero(int s, int w, int m, int p) : Edificio(s, w, m, p)
+{
+    nombre = "Aserradero";
+    codigo = "A";
+    cant_mat_que_produce = ASERRADERO_CANT_MATERIAL_PROD;
+    mat_que_produce = ASERRADERO_MATERIAL_PROD;
+}
+
+Aserradero ::~Aserradero()
+{
+}
