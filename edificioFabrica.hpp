4f99808df011b7010ba409826d16412637e2e3b9
marianoguglieri <55066532+marianoguglieri@users.noreply.github.com>
2021-11-03 10:01:57 -0300

    Create edificioFabrica.hpp

---
+++ edificioFabrica.hpp
@@ -0,0 +1,20 @@
+#ifndef EDIFICIOFABRICA_HPP
+#define EDIFICIOFABRICA_HPP
+#include "edificio.hpp"
+
+const int FABRICA_CANT_MATERIAL_PROD = 40;
+const string FABRICA_MATERIAL_PROD = "metal";
+
+class Fabrica : public Edificio
+{
+public:
+    // PRE:
+    // POS:
+    Fabrica(int s, int w, int m, int p);
+
+    // PRE:
+    // POS:
+    ~Fabrica();
+};
+
+#endif // EDIFICIOFABRICA_HPP
