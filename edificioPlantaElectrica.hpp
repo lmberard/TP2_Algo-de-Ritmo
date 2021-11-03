e47b26c37e6ba9869166755326c7aa3fa1b25415
marianoguglieri <55066532+marianoguglieri@users.noreply.github.com>
2021-11-03 10:05:25 -0300

    Create edificioPlantaElectrica.hpp

---
+++ edificioPlantaElectrica.hpp
@@ -0,0 +1,20 @@
+#ifndef EDIFICIOPLANTAELECTRICA_HPP
+#define EDIFICIOPLANTAELECTRICA_HPP
+#include "edificio.hpp"
+
+const int PLANTA_ELECTRICA_CANT_MATERIAL_PROD = 0;
+const string PLANTA_ELECTRICA_MATERIAL_PROD = "No brinda materiales";
+
+class PlantaElectrica : public Edificio
+{
+public:
+    // PRE:
+    // POS:
+    PlantaElectrica(int s, int w, int m, int p);
+
+    // PRE:
+    // POS:
+    ~PlantaElectrica();
+};
+
+#endif // EDIFICIOPLANTAELECTRICA_HPP
