0873e9bfc8fb70c240c93cc9715c3e2a9118a2fc
marianoguglieri <55066532+marianoguglieri@users.noreply.github.com>
2021-11-03 10:02:23 -0300

    Create edificioMina.cpp

---
+++ edificioMina.cpp
@@ -0,0 +1,13 @@
+#include "edificioMina.hpp"
+#include "edificio.hpp"
+Mina::Mina(int s, int w, int m, int p) : Edificio(s, w, m, p)
+{
+    nombre = "Mina";
+    codigo = "M";
+    cant_mat_que_produce = MINA_CANT_MATERIAL_PROD;
+    mat_que_produce = MINA_MATERIAL_PROD;
+}
+
+Mina::~Mina()
+{
+}
