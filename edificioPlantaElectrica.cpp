63db5b02469aa0fa8396f77d6048ec54ead57df3
marianoguglieri <55066532+marianoguglieri@users.noreply.github.com>
2021-11-03 10:04:59 -0300

    Create edificioPlantaElectrica.cpp

---
+++ edificioPlantaElectrica.cpp
@@ -0,0 +1,13 @@
+#include "edificioPlantaElectrica.hpp"
+
+PlantaElectrica::PlantaElectrica(int s, int w, int m, int p) : Edificio(s, w, m, p)
+{
+    nombre = "Planta Eléctrica";
+    codigo = "P";
+    cant_mat_que_produce = PLANTA_ELECTRICA_CANT_MATERIAL_PROD;
+    mat_que_produce = PLANTA_ELECTRICA_MATERIAL_PROD;
+}
+
+PlantaElectrica::~PlantaElectrica()
+{
+}
