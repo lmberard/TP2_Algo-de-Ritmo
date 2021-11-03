587fe26c87b98bdeb99f444cd731b29bcb7dc312
marianoguglieri <55066532+marianoguglieri@users.noreply.github.com>
2021-11-03 10:03:31 -0300

    Create edificioObelisco.cpp

---
+++ edificioObelisco.cpp
@@ -0,0 +1,12 @@
+#include "edificioObelisco.hpp"
+
+Obelisco::Obelisco(int s, int w, int m, int p) : Edificio(s, w, m, p)
+{
+    nombre = "Obelisco";
+    codigo = "O";
+    cant_mat_que_produce = OBELISCO_CANT_MATERIAL_PROD;
+    mat_que_produce = OBELISCO_MATERIAL_PROD;
+}
+Obelisco::~Obelisco()
+{
+}
