23f3d67e8ac257e8125a88edc6ce233fdee1d294
marianoguglieri <55066532+marianoguglieri@users.noreply.github.com>
2021-11-03 10:01:29 -0300

    Create edificioFabrica.cpp

---
+++ edificioFabrica.cpp
@@ -0,0 +1,13 @@
+#include "edificioFabrica.hpp"
+#include "edificio.hpp"
+Fabrica::Fabrica(int s, int w, int m, int p) : Edificio(s, w, m, p)
+{
+    nombre = "Fábrica";
+    codigo = "F";
+    cant_mat_que_produce = FABRICA_CANT_MATERIAL_PROD;
+    mat_que_produce = FABRICA_MATERIAL_PROD;
+}
+
+Fabrica::~Fabrica()
+{
+}
