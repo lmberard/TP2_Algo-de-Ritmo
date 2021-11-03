0cb001b2f86cf27de795a1e721dd0b1cdeeb54d3
marianoguglieri <55066532+marianoguglieri@users.noreply.github.com>
2021-11-03 09:52:59 -0300

    Create casilleroConstruible.cpp

---
+++ casilleroConstruible.cpp
@@ -0,0 +1,45 @@
+#include "casilleroConstruible.hpp"
+
+CasilleroConstruible ::CasilleroConstruible(int x, int y) : Casillero(x, y)
+{
+}
+
+CasilleroConstruible ::~CasilleroConstruible()
+{
+}
+
+void CasilleroConstruible ::agregar(Edificio *edificio)
+{
+    this->edificio = edificio;
+}
+
+void CasilleroConstruible ::mostrar()
+{
+    cout << "Soy un casillero construible" << endl;
+    cout << "estás en la posición " << this->coord_x << " " << this->coord_y << endl;
+    /*if (edificio)
+        cout << edificio->mostrar() << endl;*/
+}
+
+void CasilleroConstruible ::display()
+{   
+    if(!edificio)
+        cout << TXT_BLACK_16 << TXT_UNDERLINE << BGND_GREEN_2 << "   " << END_COLOR << " ";
+    else
+        cout << TXT_BLACK_16 << TXT_UNDERLINE << BGND_GREEN_2 << " " << edificio->obtener_codigo() << " " << END_COLOR << " ";
+}
+
+/*
+    void CasilleroConstruible ::construir_edificio(Ciudad andypolis, string nombre)
+    {
+        andypolis.agregar_edificio(nombre);
+        andypolis.restar_materiales(nombre, edificio);
+        // andypolis.agregar_coordenada();
+    }
+
+    void CasilleroConstruible ::destruir_edificio(Ciudad andypolis, string nombre, Edificio edificio)
+    {
+        andypolis.destruir_edificio(nombre, edificio);
+        andypolis.recolectar_materiales_reciclados(edificio);
+        // andypolis.eliminar_coordenada()
+    }*/
