6e01cd7cf9d0eb8cdcf81c36bd36ae9c404f7ca4
marianoguglieri <55066532+marianoguglieri@users.noreply.github.com>
2021-11-03 09:50:18 -0300

    Update and rename casilleros.h to casillero.hpp

---
+++ casillero.hpp
@@ -0,0 +1,43 @@
+#ifndef CASILLERO_HPP
+#define CASILLERO_HPP
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "colors.hpp"
+#include "material.hpp"
+#include "edificio.hpp"
+#include "ciudad.hpp"
+
+using namespace std;
+
+class Casillero
+{
+
+protected:
+    int coord_x = 0;
+    int coord_y = 0;
+
+public:
+    // PRE:
+    // POS:
+    Casillero(int x, int y);
+
+    // PRE:
+    // POS:
+    virtual ~Casillero(){};
+
+    // PRE:
+    // POS:
+    virtual void mostrar() = 0;
+
+    // PRE:
+    // POS:
+    virtual void display() = 0;
+
+    // PRE:
+    // POS:
+    virtual void agregar(Edificio *);
+};
+
+#endif // CASILLERO_HPP
