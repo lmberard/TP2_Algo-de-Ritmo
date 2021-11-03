614eddb6c1f4a0b293fa795306bbd4c232c7151f
marianoguglieri <55066532+marianoguglieri@users.noreply.github.com>
2021-11-03 09:51:05 -0300

    Create casillero.cpp

---
+++ casillero.cpp
@@ -0,0 +1,10 @@
+#include "casillero.hpp"
+
+Casillero::Casillero(int x, int y) : coord_x(x), coord_y(y)
+{
+}
+
+void Casillero::agregar(Edificio *)
+{
+    cout << "No puedes construir en este casillero" << endl;
+}
