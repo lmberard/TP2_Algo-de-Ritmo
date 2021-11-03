ba564f2c97aed99c471e1de97e5ecf3ad4bed4ea
marianoguglieri <55066532+marianoguglieri@users.noreply.github.com>
2021-11-03 10:13:06 -0300

    Update and rename parser.h to parser.hpp

---
+++ parser.hpp
@@ -0,0 +1,38 @@
+#ifndef PARSER_HPP
+#define PARSER_HPP
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "lista.hpp"
+#include "material.hpp"
+#include "mapa.hpp"
+#include "edificio.hpp"
+
+#include "edificioAserradero.hpp"
+#include "edificioEscuela.hpp"
+#include "edificioYacimiento.hpp"
+#include "edificioObelisco.hpp"
+#include "edificioMina.hpp"
+#include "edificioFabrica.hpp"
+#include "edificioPlantaElectrica.hpp"
+
+
+using namespace std;
+
+class Parser
+{
+
+public:
+    // PRE:
+    // POS:
+    void cargar(Lista<Edificio*> &edificios, const string &PATH);
+
+    // PRE:
+    // POS:
+    //void cargar(Lista<Material*> &materiales, const string &PATH);
+      void cargar(Ubicacion  ubicaciones[80], const string& PATH);
+    //void mostrar(const string &PATH);
+};
+
+#endif // PARSER_HPP
