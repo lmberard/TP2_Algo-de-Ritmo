6d56c63d664a80356feb21dcf4dadc4066195495
marianoguglieri <55066532+marianoguglieri@users.noreply.github.com>
2021-11-03 10:06:25 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,65 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include "mapa.h"
+#include "menu.hpp"
+#include "mapa.hpp"
+#include "material.hpp"
+#include "lista.hpp"
+#include "parser.hpp"
 
 using namespace std;
 
-int main (){
+int main()
+{
 
-  Mapa andypolis("mapa.txt");
+    /*
+    //ACA SE CREARIAN LOS VECTORES Y EL MAPA
 
-  andypolis.consultar_coordenada(0,0);
+    Parser parser;
 
-  andypolis.mostrar_mapa();
+    Lista<Edificio> edificios;
+    parser.cargar(edificios,"edificios.txt");
 
-  return 0;
+    //ESTO FALTARIA
+    // parser.cargar_ubicaciones(edificios,"ubicaciones.txt");
+
+    Lista<Material> materiales;
+    parser.cargar(materiales, "materiales.txt");
+
+    Edificio *edificio1 = edificios.consulta(1);
+    Edificio *edificio2 = edificios.consulta(2);
+
+    cout << edificio1->mostrar() << endl;
+    cout << edificio2->mostrar() << endl;
+
+    Material *material1 = materiales.consulta(1);
+    Material *material2 = materiales.consulta(2);
+
+    cout << materiales.consulta(2)->mostrar() << endl;
+
+    cout << material1->mostrar() << endl;
+    cout << material2->mostrar() << endl;
+
+    andypolis.agregar_edificio(0, 3, edificios.consulta(1));
+    andypolis.consultar_coordenada(0, 3);
+    */
+    Mapa andypolis("mapa.txt");
+    andypolis.cargar_ubicaciones("ubicaciones.txt");
+   
+    string rta = " ";
+    do
+    {
+        mostrar_InstruccionInicial();
+        rta = devolver_rta_usuario();
+        if (rta == "si")
+        {
+            mostrar_menuInicial(andypolis);
+            return 0;
+        }
+        else if (rta != "no")
+            msjeError("Error. Opciones validas: 'si' o 'no'");
+    } while (rta != "no");
+
+    msjeInstruccion("Hasta luego!! :)");
+    return 0;
 }
