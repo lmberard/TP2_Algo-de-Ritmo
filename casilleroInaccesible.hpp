110b81d46506a0423b9118d37bbf55f7a12b99c5
marianoguglieri <55066532+marianoguglieri@users.noreply.github.com>
2021-11-03 09:54:36 -0300

    Create casilleroInaccesible.hpp

---
+++ casilleroInaccesible.hpp
@@ -0,0 +1,25 @@
+#ifndef CASILLEROINACCESIBLE_HPP
+#define CASILLEROINACCESIBLE_HPP
+#include "casillero.hpp"
+
+class CasilleroInaccesible : public Casillero
+{
+public:
+    // PRE:
+    // POS:
+    CasilleroInaccesible(int x, int y);
+
+    // PRE:
+    // POS:
+    ~CasilleroInaccesible();
+
+    // PRE:
+    // POS:
+    void mostrar();
+
+    // PRE:
+    // POS:
+    void display();
+};
+
+#endif // CASILLEROINACCESIBLE_HPP
