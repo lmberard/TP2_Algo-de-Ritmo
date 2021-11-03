07790c560e55f0705d7205694ebe5bc93862d212
marianoguglieri <55066532+marianoguglieri@users.noreply.github.com>
2021-11-03 10:02:50 -0300

    Create edificioMina.hpp

---
+++ edificioMina.hpp
@@ -0,0 +1,19 @@
+#ifndef EDIFICIOMINA_HPP
+#define EDIFICIOMINA_HPP
+#include "edificio.hpp"
+
+const int MINA_CANT_MATERIAL_PROD = 15;
+const string MINA_MATERIAL_PROD = "piedra";
+
+class Mina : public Edificio
+{
+public:
+    // PRE:
+    // POS:
+    Mina(int s, int w, int m, int p);
+
+    // PRE:
+    // POS:
+    ~Mina();
+};
+#endif // EDIFICIOMINA_HPP
