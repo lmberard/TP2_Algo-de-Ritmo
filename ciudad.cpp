faffb15ce291fe65796b2528db906f152fbf8ad4
Lucia Berard <lberard@fi.uba.ar>
2021-11-05 20:01:13 -0300

    separando hpp y cpp

--- ciudad.cpp
+++ ciudad.cpp
@@ -3,7 +3,7 @@
 // CONSTRUCTOR-DESTRUCTOR--------------------------
 // FALTA REVISAR PARSER
 void Ciudad::procesar_archivos()
-{   
+{
     mapa.cargar("mapa.txt");
     mapa.cargar_ubicaciones("ubicaciones.txt");
     parser.cargar(edificios, "edificios.txt");
@@ -11,37 +11,139 @@ void Ciudad::procesar_archivos()
     cargar_ubicaciones();
 }
 
-void Ciudad::mostrar_mapa(){
+void Ciudad::mostrar_mapa()
+{
     mapa.mostrar_mapa();
 }
 
-void Ciudad::listar_edificios(){
-      for(int i = 1; i < edificios.mostrar_cantidad()+1; i++ ){
-           cout << edificios[i]->obtener_nombre() << '\t'
-                << edificios[i]->obtener_madera() << '\t'
-                << edificios[i]->obtener_metal()  << '\t'
-                << edificios[i]->obtener_piedra() << '\t'
-                << edificios[i]->obtener_cant_max() << '\t'
-                << edificios[i]->obtener_cant_max() << '\t' //ESTO HABRIA QUE RESTARLO CON CUANTOS HAY YA
-                << edificios[i]->obtener_cant_mat_producido() << ' '
-                << edificios[i]->obtener_mat_producido()    
-                << endl;
+void Ciudad::listar_edificios()
+{
+    for (int i = 1; i < edificios.mostrar_cantidad() + 1; i++)
+    {
+        cout << edificios[i]->obtener_nombre() << '\t'
+             << edificios[i]->obtener_madera() << '\t'
+             << edificios[i]->obtener_metal() << '\t'
+             << edificios[i]->obtener_piedra() << '\t'
+             << edificios[i]->obtener_cant_max() << '\t'
+             << edificios[i]->obtener_cant_max() << '\t' // ESTO HABRIA QUE RESTARLO CON CUANTOS HAY YA
+             << edificios[i]->obtener_cant_mat_producido() << ' '
+             << edificios[i]->obtener_mat_producido()
+             << endl;
     }
 }
 
-void Ciudad::mostrar_inventario(){
-    for(int i = 1; i < materiales.mostrar_cantidad()+1; i++ ){
-           cout << materiales[i]->obtener_nombre() << '\t'
-                << materiales[i]->obtener_cantidad() << ' ' 
-                << endl;
+void Ciudad::demoler_edificio(int x, int y)
+{
+    mapa.demoler_edificio(x, y);
+}
+
+int Ciudad::buscar_edificio(string edificio)
+{
+    int i = 1;
+
+    int cantidad = edificios.mostrar_cantidad();
+    while (cantidad && (edificios[i]->obtener_nombre() != edificio))
+    {
+        i++;
+        --cantidad;
+    }
+
+    return i;
+}
+
+void Ciudad::mostrar_inventario()
+{
+    for (int i = 1; i < materiales.mostrar_cantidad() + 1; i++)
+    {
+        cout << materiales[i]->obtener_nombre() << '\t'
+             << materiales[i]->obtener_cantidad() << ' '
+             << endl;
     }
 }
 
-Ciudad::~Ciudad(){
-     parser.borrar(edificios);
-     parser.borrar(materiales);
+void Ciudad::chequear_stock(int i)
+{
+    int cuenta = 0;
+    for (int j = 1; j < materiales.mostrar_cantidad() + 1; j++)
+    {
+        if (materiales[j]->obtener_nombre() == "Piedra")
+        {
+            cuenta = materiales[j]->obtener_cantidad() - edificios[i]->obtener_piedra();
+            if (cuenta < 0)
+            {
+                cout << "falta " << materiales[j]->obtener_nombre() << endl;
+                // flag = 0;
+            }
+            else
+                materiales[j]->modificar_cantidad(cuenta);
+        }
+        if (materiales[j]->obtener_nombre() == "Madera")
+        {
+            cuenta = materiales[j]->obtener_cantidad() - edificios[i]->obtener_madera();
+            if (cuenta < 0)
+            {
+                cout << "falta " << materiales[j]->obtener_nombre() << endl;
+                // flag = 0;
+            }
+            else
+                materiales[j]->modificar_cantidad(cuenta);
+        }
+        if (materiales[j]->obtener_nombre() == "Metal")
+        {
+            cuenta = materiales[j]->obtener_cantidad() - edificios[i]->obtener_metal();
+            if (cuenta < 0)
+            {
+                cout << "falta " << materiales[j]->obtener_nombre() << endl;
+                // flag = 0;
+            }
+            else
+                materiales[j]->modificar_cantidad(cuenta);
+        }
+    }
+}
+void Ciudad::construir(int x, int y, string edificio)
+{
+
+    chequear_stock(buscar_edificio(edificio));
+    // chequear_casillero()
+
+    if (edificio == "yacimiento")
+        mapa.agregar_edificio(x, y, new Yacimiento(edificios[buscar_edificio(edificio)]));
+    if (edificio == "aserradero")
+        mapa.agregar_edificio(x, y, new Aserradero(edificios[buscar_edificio(edificio)]));
+    if (edificio == "escuela")
+        mapa.agregar_edificio(x, y, new Escuela(edificios[buscar_edificio(edificio)]));
+    if (edificio == "fabrica")
+        mapa.agregar_edificio(x, y, new Fabrica(edificios[buscar_edificio(edificio)]));
+    if (edificio == "mina")
+        mapa.agregar_edificio(x, y, new Mina(edificios[buscar_edificio(edificio)]));
+    if (edificio == "obelisco")
+        mapa.agregar_edificio(x, y, new Obelisco(edificios[buscar_edificio(edificio)]));
+    if (edificio == "planta electrica")
+        mapa.agregar_edificio(x, y, new PlantaElectrica(edificios[buscar_edificio(edificio)]));
 }
 
-void Ciudad::consultar_coordenada(int x,int y){
-    mapa.consultar_coordenada(x,y);
+void Ciudad::mostrar_ubicaciones()
+{
+    mapa.mostrar_ubicaciones();
+}
+
+void Ciudad::cargar_ubicaciones()
+{
+    Ubicacion edificio;
+    for (int i = 1; i < mapa.cant_edificios() + 1; i++)
+    {
+        edificio = mapa.devolver_ubicacion(i);
+        construir(edificio.coord_x, edificio.coord_y, edificio.nombre);
+    }
+}
+Ciudad::~Ciudad()
+{
+    parser.borrar(edificios);
+    parser.borrar(materiales);
+}
+
+void Ciudad::consultar_coordenada(int x, int y)
+{
+    mapa.consultar_coordenada(x, y);
 }
--- ciudad.hpp
+++ ciudad.hpp
@@ -25,15 +25,10 @@ const int CANT_TIPOS_DE_EDIFICIOS = 6;
 class Ciudad
 {
 private:
-
     Mapa mapa;
-    Lista<Edificio*> edificios;
-    Lista<Material*> materiales;
+    Lista<Edificio *> edificios;
+    Lista<Material *> materiales;
     Parser parser;
-
-    //int cant_edificios;
-    //int cant_materiales;
-    //int cant_ubicaciones;
 
 public:
     // CONSTRUCTOR-DESTRUCTOR--------------------------
@@ -42,7 +37,7 @@ public:
     void procesar_archivos();
     // PRE:
     // POST: se construye la instancia de Ciudad con listas vacias y cantidades=0
-   // Ciudad();
+    // Ciudad();
     // PRE:
     // POST: se destruyen las listas, clases y la instancia de Ciudad.
     ~Ciudad();
@@ -50,11 +45,11 @@ public:
     // FUNCIONES DEL MENU PRINCIPAL--------------------
     // PRE:
     // POST: Se construye un edificio segun su nombre y coordenadas en el mapa. Se modifica la cantidad de materiales que se usaron y se agrega a la lista de ubicaciones
-    //void construir_edificio(string nombre, int x, int y);
+    // void construir_edificio(string nombre, int x, int y);
 
     // PRE:
     // POST: ??
-   // void listar_edificios_construidos();
+    // void listar_edificios_construidos();
 
     // PRE:
     // POST: Se recorre la lista de edificios y se muestra por la terminal con sus respectivos atributos
@@ -62,9 +57,7 @@ public:
 
     // PRE:
     // POST: se elimina un edificio segun su nombre y coordenadas del mapa. Se recolectan los materiales reciclados, se agregan a la lista y se elimina la ubicacion de la lista de ubicaciones
-    void demoler_edificio(int x, int y){
-        mapa.demoler_edificio(x,y);
-    }
+    void demoler_edificio(int x, int y);
 
     // PRE:
     // POST: se recorre el mapa y se muestra por la terminal todos los casillero coloreados segun el tipo de casillero y lo que contienen (material o edificio)
@@ -74,135 +67,42 @@ public:
     // POST: se imprime por la terminal que tipo de casillero es ese y que tiene adentro (material/edificio)
     void consultar_coordenada(int x, int y);
 
+    // LISTAR TODOS LOS CONSTRUIDOS
     // PRE:
     // POST: se recorre la lista de materiales y se muestran por la terminal con su respectiva cantidad
     void mostrar_inventario();
 
     // PRE:
+    // POST:
+    int buscar_edificio(string edificio);
+
+    // PRE:
+    // POST:
+    void chequear_stock(int i);
+
+    // PRE:
+    // POST:
+    void construir(int x, int y, string edificio);
+
+    // PRE:
+    // POST:
+    void cargar_ubicaciones();
+
+    // PRE:
+    // POST:
+    void mostrar_ubicaciones();
+
+    // PRE:
     // POST: se recolectan los recursos producidos de los edificios construidos y se guardan en la lista de materiales
-    //void recolectar_recursos();
+    // void recolectar_recursos();
 
     // PRE:
     // POST: se crean materiales nuevos aleatoriamente y se colocan en los casilleros transitorios y luego se guardan en la lista de materiales(opcional?)
-    //void lluvia_recursos();
+    // void lluvia_recursos();
 
     // PRE:
     // POST: se guarda lo de la lista de edificios, materiales y ubicaciones en sus archivos txt
-    //void guardar_archivos();
-
-    // FUNCIONES AUXILIARES----------------------------
-
-    /////// LLUVIA DE RECURSOS
-    // POST: se agrega el material y su cantidad en el casillero de coordenada (x,y) y en la lista de materiales
-    //void agregar_material_en_coordenada(string nombre_material, int cantidad, int x, int y);
-
-    /////// CONSTRUIR EDIFICIO
-    // PRE:
-    // POST: Construye los edificios de la lista de ubicaciones
-    //void inicializar_mapa();
-    // PRE:
-    // POST: construye el edificio en la coordenada (x,y)
-    //void agregar_edificio(string nombre_edificio, int x, int y);
-    // PRE:
-    // POST: resta los materiales necesarios para la construccion segun el nombre del edificio y los elimina de la lista de materiales
-    //void restar_materiales_construccion(string nombre_edificio);
-    // PRE:
-    // POST: se agrega la ubicacion del edificio en la lista de ubicaciones segun su nombre y coordenada
-    //void agregar_ubicacion(string nombre_edificio, int x, int y);
-    // PRE:
-    // POST: se agrega la ubicacion del edificio en la lista de ubicaciones segun lo que hay en el casillero
-    //void agregar_ubicacion_contenido_casillero();
-
-    /////// DESTRUIR EDIFICIO
-    // PRE:
-    // POST: se destruye el edificio segun el nombre y coordenadas
-    //void destruir_edificio(string nombre_edificio, int x, int y);
-    // PRE:
-    // POST: se recolecta la mitad de los materiales utilizados para construir ese edificio.
-    //void recolectar_materiales_reciclados(string nombre_edificio);
-    // PRE:
-    // POST: se elimina la ubicacion de la lista de ubicaciones
-    //void eliminar_ubicacion(string nombre_edificio, int x, int y);
-
-    int buscar_edificio(string edificio){
-        int i = 1;
-       
-        int cantidad = edificios.mostrar_cantidad();
-        while(cantidad && (edificios[i]->obtener_nombre() != edificio)){
-            i++;
-            --cantidad;
-        }
-        
-        return i;
-    }
-
-    void chequear_stock(int i){
-        int cuenta = 0;
-        for(int j = 1; j < materiales.mostrar_cantidad()+1; j++ ){
-            if(materiales[j]->obtener_nombre() == "Piedra"){  
-                cuenta = materiales[j]->obtener_cantidad() - edificios[i]->obtener_piedra();
-                if(cuenta < 0 ){
-                    cout << "falta " << materiales[j]->obtener_nombre() << endl;
-                    //flag = 0;
-                }
-                else
-                    materiales[j]->modificar_cantidad(cuenta);
-            }
-            if(materiales[j]->obtener_nombre() == "Madera"){
-                cuenta = materiales[j]->obtener_cantidad() - edificios[i]->obtener_madera();
-                if(cuenta < 0 ){
-                    cout << "falta " << materiales[j]->obtener_nombre() << endl;
-                    //flag = 0;
-                }
-                else
-                    materiales[j]->modificar_cantidad(cuenta);
-            }
-            if(materiales[j]->obtener_nombre() == "Metal"){
-                cuenta = materiales[j]->obtener_cantidad() - edificios[i]->obtener_metal();
-                if(cuenta < 0 ){
-                    cout << "falta " << materiales[j]->obtener_nombre() << endl;
-                    //flag = 0;
-                }
-                else
-                    materiales[j]->modificar_cantidad(cuenta);
-            }
-        }
-    }
-
-    void construir(int x, int y, string edificio){      
-        
-        chequear_stock(buscar_edificio(edificio));
-        //chequear_casillero()
-
-        if(edificio == "yacimiento")
-            mapa.agregar_edificio(x,y, new Yacimiento(edificios[buscar_edificio(edificio)]));
-        if(edificio == "aserradero")
-            mapa.agregar_edificio(x,y, new Aserradero(edificios[buscar_edificio(edificio)]));
-        if(edificio == "escuela")
-            mapa.agregar_edificio(x,y, new Escuela(edificios[buscar_edificio(edificio)]));
-        if(edificio == "fabrica")
-            mapa.agregar_edificio(x,y, new Fabrica(edificios[buscar_edificio(edificio)]));
-        if(edificio == "mina")
-            mapa.agregar_edificio(x,y, new Mina(edificios[buscar_edificio(edificio)]));
-        if(edificio == "obelisco")
-            mapa.agregar_edificio(x,y, new Obelisco(edificios[buscar_edificio(edificio)]));
-        if(edificio == "planta electrica")
-            mapa.agregar_edificio(x,y, new PlantaElectrica(edificios[buscar_edificio(edificio)]));
-    }
-
-    void cargar_ubicaciones(){
-        Ubicacion edificio; 
-        for(int i = 1; i < mapa.cant_edificios()+1;i++){
-            edificio = mapa.devolver_ubicacion(i);
-            construir(edificio.coord_x, edificio.coord_y, edificio.nombre);
-        }
-    }
-
-    void mostrar_ubicaciones(){
-        mapa.mostrar_ubicaciones();
-    }
-    
+    // void guardar_archivos();
 };
-
 
 #endif // CIUDAD_HPP
