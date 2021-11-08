ac58df81dea74a7f64e6f12fc9fa98328f1cbdf5
marianoguglieri <55066532+marianoguglieri@users.noreply.github.com>
2021-11-08 11:27:26 -0300

    Update casillero.cpp

--- casillero.cpp
+++ casillero.cpp
@@ -2,7 +2,20 @@
 
 Casillero::Casillero(){}
 
-void Casillero::agregar(Edificio *)
+bool Casillero::agregar(Edificio *)
 {
     cout << "No puedes construir en este casillero" << endl;
+    return NULL;
+}
+
+string Casillero::obtener_material(){
+    return "C";
+}
+
+int Casillero::obtener_cantidad(){
+    return 0;
+}
+
+Edificio* Casillero::mostrar_edificio(){
+    return NULL;
 }
