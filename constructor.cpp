f564909ee78d2147dd966e333176d8442ad5ef4b
marianoguglieri <55066532+marianoguglieri@users.noreply.github.com>
2021-11-08 11:36:24 -0300

    Update constructor.cpp

--- constructor.cpp
+++ constructor.cpp
@@ -1,8 +1,6 @@
 #include "constructor.hpp"
 
-Constructor::Constructor(){}
-
-void Constructor::cargar(const string & PATH)
+Constructor::Constructor(const string & PATH)
 {
     fstream archivo_edificios(PATH, ios::in);
 
@@ -43,41 +41,44 @@ void Constructor::cargar(const string & PATH)
 Edificio * Constructor::construye(const string & edificio){
 
     Edificio * construido = NULL;
+    int pos = buscar_edificio(edificio);
+
+    if(pos > edificios.mostrar_cantidad())
+        return construido;
 
     if(edificio == "yacimiento")
-        construido = new Yacimiento(edificios[buscar_edificio(edificio)]);
+        construido = new Yacimiento(edificios[pos]);
     if(edificio == "aserradero")
-        construido = new Aserradero(edificios[buscar_edificio(edificio)]);
+        construido = new Aserradero(edificios[pos]);
     if(edificio == "escuela")
-        construido = new Escuela(edificios[buscar_edificio(edificio)]);
+        construido = new Escuela(edificios[pos]);
     if(edificio == "fabrica")
-        construido = new Fabrica(edificios[buscar_edificio(edificio)]);
+        construido = new Fabrica(edificios[pos]);
     if(edificio == "mina")
-        construido = new Mina(edificios[buscar_edificio(edificio)]);
+        construido = new Mina(edificios[pos]);
     if(edificio == "obelisco")
-        construido = new Obelisco(edificios[buscar_edificio(edificio)]);
+        construido = new Obelisco(edificios[pos]);
     if(edificio == "planta electrica")
-        construido = new PlantaElectrica(edificios[buscar_edificio(edificio)]);
+        construido = new PlantaElectrica(edificios[pos]);
 
     return construido;
 }
 
 int Constructor::buscar_edificio(string edificio)
 {
-    int i = 1;
-       
+    int pos = 1;
     int cantidad = edificios.mostrar_cantidad();
-    while(cantidad && (edificios[i]->obtener_nombre() != edificio)){
-        i++;
+    
+    while(cantidad && (edificios[pos]->obtener_nombre() != edificio)){
+        pos++;
         --cantidad;
     }
-        
-    return i;
+
+    return pos;
 }
 
-void Constructor::borrar()
+Constructor::~Constructor()
 {
-
 	for(int i = 1; i < edificios.mostrar_cantidad()+1; i++ ){
         delete edificios[i];
     }
