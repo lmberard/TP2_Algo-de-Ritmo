fc498b3e9930949f1861d410efd5b642a0668218
marianoguglieri <55066532+marianoguglieri@users.noreply.github.com>
2021-11-08 11:35:36 -0300

    Update ciudad.hpp

--- ciudad.hpp
+++ ciudad.hpp
@@ -13,37 +13,34 @@ struct Provision{
 
 class Ciudad{
 
-    Terreno terreno;
-    Constructor bob;
-    Recurso recurso;
-
     Lista<Provision> provisiones;
     Lista<Ubicacion> ubicaciones;
 
+    Terreno terreno;
     int filas;
     int columnas;
     Casillero ***mapa;
 
     public:
         Ciudad();
-        Ciudad(const string &);
+        Ciudad(const string &,const string &, const string &,Constructor & bob, Recurso & recurso);
         ~Ciudad();
 
         void mostrar_mapa();
 
         void consultar_coordenada(int , int );
      
-        void construir(int ,int ,const string & );
+        void construir(int ,int ,const string &, Constructor & bob );
 
-        void cargar_ubicaciones();
+        void cargar_ubicaciones(Constructor & bob);
 
-        bool chequear_permisos_edificio(const string & edificio);
+        bool chequear_permisos_edificio(const string & edificio, Constructor & bob);
 
-        bool chequear_stock(Edificio * edificio);
+        bool chequear_stock(Edificio * edificio, bool);
 
         void cargar_ubicaciones(const string& PATH);
 
-        void cargar_provisiones(const string & PATH);
+        void cargar_provisiones(const string & PATH, Recurso & recurso);
 
         void mostrar_inventario();
 
@@ -56,4 +53,23 @@ class Ciudad{
         void demoler_todo();
 
         void mostrar_ubicaciones();
+
+        int construidos(const string &);
+
+        void recolectar();
+
+        void guardar_materiales();
+
+        void quitar_ubicacion(int x,int y);
+
+        void lluvia(Recurso &){
+            //tener un atributo que sea coordenadas transitables y cargarlo
+            //para esto habría que generar en casillero una funcion del tipo bool
+            //que sea algo como CasilleroTransitable? si:no. o algo asi no se 
+            //sacar un numero random para agarrar un par de coordenadas del vector 
+            //instanciar un material con la clase recurso  
+            //Borrar ese par de coordenadas para que no salga de nuevo 
+            //Si se llenan todos los casilleros decir mensaje 
+        }
+
 };
